a7a46306687f9a9b7fb93f040f4b4aa5de975085
parkyounghwan <dudrnxps1@gmail.com>
2020-06-30 01:23:12 +0900

    #yh-issue-2, mediainfo

--- src/fileUpload.js
+++ src/fileUpload.js
@@ -1,5 +1,6 @@
-(() => {
+const MediaInfo = require('mediainfo.js');
 
+(() => {
   const holder = document.getElementById("drag-file");
   const video = document.querySelector("#drag-file > video");
   
@@ -19,16 +20,56 @@
     event.preventDefault();
     
     for (let file of event.dataTransfer.files) {
-
-      console.log("File(s) you dragged here: ", file.path)
       
       video.src = file.path;
       video.hidden = false;
+
+      console.log("file: ", file);
+
+      MediaInfo({ format: 'object' }, (mediainfo) => {
+        (() => {
+          onChangeFile(file, mediainfo);
+        })();
+      })
 
       document.querySelector("#drag-file > h1").hidden = true;
     }
 
     return false;
   };
-
 })();
+
+const onChangeFile = (file, mediainfo) => {
+  if (file) {
+    // output.value = 'Working…'
+
+    const getSize = () => file.size
+
+    const readChunk = (chunkSize, offset) =>
+      new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        reader.onload = (event) => {
+          if (event.target.error) {
+            reject(event.target.error)
+          }
+          resolve(new Uint8Array(event.target.result))
+        }
+        reader.readAsArrayBuffer(file.slice(offset, offset + chunkSize))
+      })
+
+    mediainfo
+      .analyzeData(getSize, readChunk)
+      .then((result) => {
+        // output.value = result
+        console.log(result);
+        console.log(result.media.track[0]);
+        console.log("duration: ", result.media.track[0].Duration);
+        console.log("FrameCount: ", result.media.track[0].FrameCount);
+        console.log("FrameRate: ", result.media.track[0].FrameRate);
+      })
+      .catch((error) => {
+        // output.value = `An error occured:\n${error.stack}`
+        console.log(error.stack);
+      })
+  }
+}
--- src/keyboardControl.js
+++ src/keyboardControl.js
@@ -4,11 +4,32 @@ window.addEventListener("keypress", (event) => {
   const video = document.querySelector("#drag-file > video");
   const frameNumberTag = document.getElementById("frame-number");
 
+  console.log("event.keyCode: ", event.keyCode);
+
   if (video.hidden) {
     return false;
   }
 
   const frameNumber = getCurrentVideoFrame(video, fps);
+
+  if (event.keyCode == 113) {
+    const videoPath = video.src;
+
+    const video1 = document.createElement("video");
+    video1.src = videoPath;
+
+    console.log(video1.files);
+
+    // if (file) {
+    //   MediaInfo({ format: 'object' }, (mediainfo) => {
+    //     (() => {
+    //       console.log("key board control");
+    //       onChangeFile(fileObj, mediainfo);
+    //     })();
+    //   })
+    // }
+
+  }
 
   if (event.keyCode == 32) {
     frameNumberTag.innerText = frameNumber;
@@ -38,3 +59,37 @@ const getCurrentVideoFrame = (video, frameRate) => {
 
   return Math.floor(curTime * frameRate);
 };
+
+// const onChangeFile = (file, mediainfo) => {
+//   if (file) {
+
+//     const getSize = () => file.size
+
+//     const readChunk = (chunkSize, offset) =>
+//       new Promise((resolve, reject) => {
+//         const reader = new FileReader()
+//         reader.onload = (event) => {
+//           if (event.target.error) {
+//             reject(event.target.error)
+//           }
+//           resolve(new Uint8Array(event.target.result))
+//         }
+//         reader.readAsArrayBuffer(file.slice(offset, offset + chunkSize))
+//       })
+
+//     mediainfo
+//       .analyzeData(getSize, readChunk)
+//       .then((result) => {
+//         // output.value = result
+//         console.log(result);
+//         console.log(result.media.track[0]);
+//         console.log("duration: ", result.media.track[0].Duration);
+//         console.log("FrameCount: ", result.media.track[0].FrameCount);
+//         console.log("FrameRate: ", result.media.track[0].FrameRate);
+//       })
+//       .catch((error) => {
+//         // output.value = `An error occured:\n${error.stack}`
+//         console.log(error.stack);
+//       })
+//   }
+// }
